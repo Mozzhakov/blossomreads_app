43b4aa1c88fb32bea935e3a2b5e0d2ecb1f1fd93
Mozzhakov <imozzhakov@icloud.com>
2023-10-05 17:43:24 +0300

    added new node env

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,8 +1,11 @@
+import LoginForm from "./LoginForm";
+
 export default function WelcomePage({ name }) {
   return (
     <>
       <h1>Welcome, {name}</h1>
       <p>Here will be user’s content</p>
+      <LoginForm />
     </>
   );
 }
