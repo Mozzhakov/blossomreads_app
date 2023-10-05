b2b36a77aa245f23ea9421b96faece1626fd71d3
Mozzhakov <imozzhakov@icloud.com>
2023-10-05 17:18:47 +0300

    added new node env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NODE_ENV: "production",
-  },
+  //   env: {
+  //     NODE_ENV: "production",
+  //   },
 };
 
 module.exports = nextConfig;
