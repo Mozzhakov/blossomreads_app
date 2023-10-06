3e740aa13d6345d58d00b89f512c5a919365ec7b
Mozzhakov <imozzhakov@icloud.com>
2023-10-06 10:27:11 +0300

    changed path to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../images/stastiem-logo.webp";
+// import logo from "../images/stastiem-logo.webp";
 // import logo from "@/images/stastiem-logo.webp";
 import styles from "../styles/header.module.css";
 
@@ -21,7 +21,7 @@ function Header() {
   return (
     <header className={styles.header}>
       <Link href="/" className="logo">
-        <Image src={logo} alt="Logo" width={70} height={42} />
+        <Image src="stastim-logo.webp" alt="Logo" width="70" height="42" />
       </Link>
       <nav>
         <div>
