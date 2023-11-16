3a264878fa6b9433416197a1815f48a2c32941eb
Mozzhakov <imozzhakov@icloud.com>
2023-11-16 11:15:54 +0200

    added story editing modal with logic and styles

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -30,26 +30,28 @@ export default function OrderList({ orders }) {
     return formattedDate;
   }
   return (
-    <section className={styles["order-list-section"]}>
-      <h4 className={styles["order-list-title"]}>Your orders</h4>
-      <ul className={styles["order-list"]}>
-        {orders.map((el) => (
-          <li key={el.order_id} className={styles["order-item"]}>
-            <Link href={`/orders/${el.order_id}`}>
-              <Image
-                src={cover}
-                alt={el.hero_name}
-                width={300}
-                className={styles["order-item-image"]}
-                priority={false}
-              />
-            </Link>
-            <p className={styles["order-item-date"]}>
-              {formatDate(el.created_at)}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </section>
+    orders && (
+      <section className={styles["order-list-section"]}>
+        <h4 className={styles["order-list-title"]}>Your orders</h4>
+        <ul className={styles["order-list"]}>
+          {orders.map((el) => (
+            <li key={el.order_id} className={styles["order-item"]}>
+              <Link href={`/orders/${el.order_id}`}>
+                <Image
+                  src={cover}
+                  alt={el.hero_name}
+                  width={300}
+                  className={styles["order-item-image"]}
+                  priority={false}
+                />
+              </Link>
+              <p className={styles["order-item-date"]}>
+                {formatDate(el.created_at)}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
   );
 }
