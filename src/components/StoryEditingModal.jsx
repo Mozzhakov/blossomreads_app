4205d89b1c20b2257287397973219ceccfff4ac1
Mozzhakov <imozzhakov@icloud.com>
2023-12-07 09:23:20 +0200

    vh experiment

--- src/components/StoryEditingModal.jsx
+++ src/components/StoryEditingModal.jsx
@@ -7,7 +7,7 @@ import { UndoIcon, RedoIcon, DoneIcon } from "@/components/Icons";
 import styles from "../scss/story-details.module.scss";
 import useUndoableState from "@/hooks/UseUndoableState";
 // import useDynamicVH from "@/hooks/useDynamicVH";
-// import Textarea from "./Textarea";
+import Textarea from "./Textarea";
 
 export const StoryEditingModal = ({
   order_id,
@@ -66,14 +66,14 @@ export const StoryEditingModal = ({
   return (
     // <div style={{ width: "100%" }}>
     <form style={{ width: "100%" }} onSubmit={onDoneClick}>
-      {/* <Textarea val={doc} fn={handleChange} styles={styles} /> */}
-      <textarea
+      <Textarea val={doc} fn={handleChange} styles={styles} />
+      {/* <textarea
         name="textarea"
         onChange={handleChange}
         value={doc}
         autoFocus
         className={styles["story-textarea"]}
-      ></textarea>
+      ></textarea> */}
       <div className={styles["story-bottom-panel"]}>
         <div className={styles["story-btn-wrapper"]}>
           <button
